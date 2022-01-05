ddb4430aa9a255bf1a52ff93f0c4bade44c6154a
Mike Wall <daikazu@gmail.com>
2022-01-05 14:17:13 -0500

    regression in picture tag

--- resources/views/components/picture.blade.php
+++ resources/views/components/picture.blade.php
@@ -6,9 +6,7 @@
 {{ Glider::url($src.'?p=lg-webp')  }} 1280w,
 {{ Glider::url($src.'?p=xl-webp')  }} 1440w,
 {{ Glider::url($src.'?p=2xl-webp')  }} 1680w"
-    sizes="1px"
-    onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
-    type="image/webp"
+            type="image/webp"
     >
     <source srcset="
 {{ Glider::url($src.'?p=xs')  }} 320w,
@@ -17,9 +15,7 @@
 {{ Glider::url($src.'?p=lg')  }} 1280w,
 {{ Glider::url($src.'?p=xl')  }} 1440w,
 {{ Glider::url($src.'?p=2xl')  }} 1680w"
-    sizes="1px"
-    onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
-    type="{{ $mime_type }}"
+            type="{{ $mime_type }}"
     >
     <img src="{{ Glider::url($src.'?p=lg')  }}"
         {{ $attributes }}>
