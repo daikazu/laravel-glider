e7f2292e42aad63285562e685216e1752b611659
Mike Wall <daikazu@gmail.com>
2022-01-05 13:27:36 -0500

    Programatically calculate sizes in srcsets

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -1,4 +1,3 @@
-@props(['sizes' => '(min-width: 320px) 75vw, 90vw'])
 @if($responsive)
 <image srcset="
 {{ Glider::url($src.'?p=xs')  }} 320w,
@@ -7,8 +6,9 @@
 {{ Glider::url($src.'?p=lg')  }} 1280w,
 {{ Glider::url($src.'?p=xl')  }} 1440w,
 {{ Glider::url($src.'?p=2xl')  }} 1680w"
-           sizes="{{ $sizes }}"
-           src="{{ Glider::url($src.'?p=lg')  }}"
+           sizes="1px"
+           onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+           src="{{ Glider::url($src.'?p=lg')}}"
     {{ $attributes }}>
 @else
 <img src="{{ Glider::url($src) }}" {{ $attributes }}>
--- resources/views/components/picture.blade.php
+++ resources/views/components/picture.blade.php
@@ -1,4 +1,3 @@
-@props(['sizes' => '(min-width: 320px) 75vw, 90vw'])
 <picture>
     <source srcset="
 {{ Glider::url($src.'?p=xs-webp')  }} 320w,
@@ -7,7 +6,8 @@
 {{ Glider::url($src.'?p=lg-webp')  }} 1280w,
 {{ Glider::url($src.'?p=xl-webp')  }} 1440w,
 {{ Glider::url($src.'?p=2xl-webp')  }} 1680w"
-    sizes="{{ $sizes }}"
+    sizes="1px"
+    onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
     type="image/webp"
     >
     <source srcset="
@@ -17,8 +17,9 @@
 {{ Glider::url($src.'?p=lg')  }} 1280w,
 {{ Glider::url($src.'?p=xl')  }} 1440w,
 {{ Glider::url($src.'?p=2xl')  }} 1680w"
-            sizes="{{ $sizes }}"
-            type="{{ $mime_type }}"
+    sizes="1px"
+    onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+    type="{{ $mime_type }}"
     >
     <img src="{{ Glider::url($src.'?p=lg')  }}"
         {{ $attributes }}>
