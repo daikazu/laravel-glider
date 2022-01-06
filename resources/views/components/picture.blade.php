0961578463dc4ce2d93328aa244d21a601657fed
Mike Wall <daikazu@gmail.com>
2022-01-05 23:22:24 -0500

    programatic sizes attribute

--- resources/views/components/picture.blade.php
+++ resources/views/components/picture.blade.php
@@ -1,22 +1,38 @@
+@once
+    <script>
+        window.responsiveResizeObserver = new ResizeObserver((entries) => {
+            entries.forEach(entry => {
+                const imgWidth = entry.target.getBoundingClientRect().width;
+                entry.target.parentNode.querySelectorAll('source').forEach((source) => {
+                    source.sizes = Math.ceil(imgWidth / window.innerWidth * 100) + 'vw';
+                });
+            });
+        });
+    </script>
+@endonce
+
 <picture>
     <source srcset="
-{{ Glider::url($src.'?p=xs-webp')  }} 320w,
-{{ Glider::url($src.'?p=sm-webp')  }} 480w,
-{{ Glider::url($src.'?p=md-webp')  }} 768w,
-{{ Glider::url($src.'?p=lg-webp')  }} 1280w,
-{{ Glider::url($src.'?p=xl-webp')  }} 1440w,
-{{ Glider::url($src.'?p=2xl-webp')  }} 1680w"
+{{ Glider::url($src.'?p=xs-webp') }} 320w,
+{{ Glider::url($src.'?p=sm-webp') }} 480w,
+{{ Glider::url($src.'?p=md-webp') }} 768w,
+{{ Glider::url($src.'?p=lg-webp') }} 1280w,
+{{ Glider::url($src.'?p=xl-webp') }} 1440w,
+{{ Glider::url($src.'?p=2xl-webp') }} 1680w"
+            sizes="1px"
             type="image/webp"
     >
     <source srcset="
-{{ Glider::url($src.'?p=xs')  }} 320w,
-{{ Glider::url($src.'?p=sm')  }} 480w,
-{{ Glider::url($src.'?p=md')  }} 768w,
-{{ Glider::url($src.'?p=lg')  }} 1280w,
-{{ Glider::url($src.'?p=xl')  }} 1440w,
-{{ Glider::url($src.'?p=2xl')  }} 1680w"
+{{ Glider::url($src.'?p=xs') }} 320w,
+{{ Glider::url($src.'?p=sm') }} 480w,
+{{ Glider::url($src.'?p=md') }} 768w,
+{{ Glider::url($src.'?p=lg') }} 1280w,
+{{ Glider::url($src.'?p=xl') }} 1440w,
+{{ Glider::url($src.'?p=2xl') }} 1680w"
+            sizes="1px"
             type="{{ $mime_type }}"
     >
-    <img src="{{ Glider::url($src.'?p=lg')  }}"
+    <img src="{{ Glider::url($src.'?p=lg') }}"
+         onload="this.onload=null;window.responsiveResizeObserver.observe(this);"
         {{ $attributes }}>
 </picture>
