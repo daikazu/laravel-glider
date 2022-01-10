c93272f9c87a4bb2c2c1875c728afb279751cfdf
Mike Wall <daikazu@gmail.com>
2022-01-10 16:11:48 -0500

    check against image size attributes

--- resources/views/components/picture.blade.php
+++ resources/views/components/picture.blade.php
@@ -12,27 +12,34 @@
 @endonce
 
 <picture>
-    <source srcset="
-{{ Glider::url($src.'?p=xs-webp') }} 320w,
-{{ Glider::url($src.'?p=sm-webp') }} 480w,
-{{ Glider::url($src.'?p=md-webp') }} 768w,
-{{ Glider::url($src.'?p=lg-webp') }} 1280w,
-{{ Glider::url($src.'?p=xl-webp') }} 1440w,
-{{ Glider::url($src.'?p=2xl-webp') }} 1680w"
-            sizes="1px"
-            type="image/webp"
-    >
-    <source srcset="
-{{ Glider::url($src.'?p=xs') }} 320w,
-{{ Glider::url($src.'?p=sm') }} 480w,
-{{ Glider::url($src.'?p=md') }} 768w,
-{{ Glider::url($src.'?p=lg') }} 1280w,
-{{ Glider::url($src.'?p=xl') }} 1440w,
-{{ Glider::url($src.'?p=2xl') }} 1680w"
-            sizes="1px"
-            type="{{ $mime_type }}"
-    >
-    <img src="{{ Glider::url($src.'?p=lg') }}"
-         onload="this.onload=null;window.responsiveResizeObserver.observe(this);"
-        {{ $attributes }}>
+<source srcset="
+@if($attributes->get('width') >= '320'){{ Glider::url($src.'?p=xs-webp') }} 320w @endif
+@if($attributes->get('width') >= '480'), {{ Glider::url($src.'?p=sm-webp') }} 480w @endif
+@if($attributes->get('width') >= '768'), {{ Glider::url($src.'?p=md-webp') }} 768w @endif
+@if($attributes->get('width') >= '1280'), {{ Glider::url($src.'?p=lg-webp') }} 1280w @endif
+@if($attributes->get('width') >= '1440'), {{ Glider::url($src.'?p=xl-webp') }} 1440w @endif
+@if($attributes->get('width') >= '1680'), {{ Glider::url($src.'?p=2xl-webp') }} 1680w @endif
+"
+sizes="1px"
+type="image/webp"
+>
+<source srcset="
+@if($attributes->get('width') >= '320'){{ Glider::url($src.'?p=xs') }} 320w @endif
+@if($attributes->get('width') >= '480'), {{ Glider::url($src.'?p=sm') }} 480w @endif
+@if($attributes->get('width') >= '768'), {{ Glider::url($src.'?p=md') }} 768w @endif
+@if($attributes->get('width') >= '1280'), {{ Glider::url($src.'?p=lg') }} 1280w @endif
+@if($attributes->get('width') >= '1440'), {{ Glider::url($src.'?p=xl') }} 1440w @endif
+@if($attributes->get('width') >= '1680'), {{ Glider::url($src.'?p=2xl') }} 1680w @endif
+"
+sizes="1px"
+type="{{ $mime_type }}"
+>
+<img
+@if($attributes->has('width') and $attributes->has('height'))
+src="{{ Glider::url($src.'?w='.$attributes->get('width').'&h='.$attributes->get('height').'&fit=crop')}}"
+@else
+src="{{ Glider::url($src.'?p=lg')}}"
+@endif"
+onload="this.onload=null;window.responsiveResizeObserver.observe(this);"
+{{ $attributes }}>
 </picture>
